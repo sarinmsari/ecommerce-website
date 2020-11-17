c3b52f59419601ae81a4c01a6df6cc132681950d
Sarin M <Sarinmsari@gmail.com>
2020-11-17 19:07:04 +0530

    user login,signup and logout added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var fileUpload=require('express-fileupload')
 var db=require('./config/connection');
+var session=require('express-session')
 
 var indexRouter = require("./routes/index");
 var adminRouter = require("./routes/admin");
@@ -29,6 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload())
+app.use(session({secret:"Key",cookie:{maxAge:600000}}))
 
 db.connect((err)=>{
   if(err){
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,45 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 var productHelper=require('../helpers/productHelper')
+const userHelper=require('../helpers/userHelper')
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let user=req.session.user
+  console.log(user);
   productHelper.getAllProducts().then((products)=>{
-    console.log(products);
-    res.render('user/viewProduct',{admin:true, products});
+    res.render('user/viewProduct',{ products,user});
   })
 
 });
+
+router.get('/login',(req,res,next)=>{
+  res.render("user/login")
+})
+router.get('/signup',(req,res,next)=>{
+  res.render("user/signup")
+})
+router.post('/signup',(req,res,next)=>{
+  userHelper.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
+})
+router.post('/login',(req,res,next)=>{
+  userHelper.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true;
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/login')
+})
 
 module.exports = router;

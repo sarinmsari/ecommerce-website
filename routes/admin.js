5d41258fcff5d12f6966e3b3c8aecd6c55a0b6ec
Sarin M <Sarinmsari@gmail.com>
2020-11-20 13:06:02 +0530

    image update activated for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,51 +1,50 @@
-var express = require('express');
-const { response } = require('../app');
+var express = require("express");
+const { response } = require("../app");
 var router = express.Router();
-var productHelper=require('../helpers/productHelper')
-
+var productHelper = require("../helpers/productHelper");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  productHelper.getAllProducts().then((products)=>{
-    res.render('admin/viewProducts',{admin:true, products});
-  })
+router.get("/", function (req, res, next) {
+  productHelper.getAllProducts().then((products) => {
+    res.render("admin/viewProducts", { admin: true, products });
+  });
 });
-router.get('/addProduct',(req,res,next)=>{
-  res.render('admin/addProduct',{admin:true});
-})
-router.post('/addProduct',(req,res,next)=>{
-  
-  productHelper.addProduct(req.body,(id)=>{
-    let image=req.files.Image
-    image.mv('./public/productImages/'+id+'.jpg',(err,done)=>{
-      if(!err){
-        res.render("admin/addProduct")
-      }else{
+router.get("/addProduct", (req, res, next) => {
+  res.render("admin/addProduct", { admin: true });
+});
+router.post("/addProduct", (req, res, next) => {
+  productHelper.addProduct(req.body, (id) => {
+    let image = req.files.Image;
+    image.mv("./public/productImages/" + id + ".jpg", (err, done) => {
+      if (!err) {
+        res.render("admin/addProduct");
+      } else {
         console.log(err);
       }
-    })
-  })
-})
-router.get('/editProduct/:id',async (req,res,next)=>{
-  let product=await productHelper.getProductDetails(req.params.id)
-  res.render('./admin/editProduct',{admin:true,product})
+    });
+  });
+});
+router.get("/editProduct/:id", async (req, res, next) => {
+  let product = await productHelper.getProductDetails(req.params.id);
+  res.render("./admin/editProduct", { admin: true, product });
   console.log(product);
+});
+router.post("/editProduct/:id", (req, res) => {
+  productHelper.updateProduct(req.params.id, req.body).then(() => {
+    res.redirect("/admin/");
+    if (req.files.Image) {
+      let image = req.files.Image;
+      image.mv("./public/productImages/" + req.params.id +".jpg")
+    }
+  });
+});
 
-})
-router.post('/editProduct/:id',(req,res)=>{
-  productHelper.updateProduct(req.params.id,req.body).then(()=>{
-    res.redirect('/admin/')
-  })
-})
-
-router.get('/deleteProduct/:id',(req,res,next)=>{
-  let proId=req.params.id
+router.get("/deleteProduct/:id", (req, res, next) => {
+  let proId = req.params.id;
   console.log(proId);
-  productHelper.deleteProduct(proId).then((response)=>{
-    res.redirect('/admin/')
-  })
-})
-
-
+  productHelper.deleteProduct(proId).then((response) => {
+    res.redirect("/admin/");
+  });
+});
 
 module.exports = router;

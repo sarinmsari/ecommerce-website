b90252379db1aa516d1d8b659d4dcd6ea59cf157
Sarin M <Sarinmsari@gmail.com>
2020-11-22 22:21:03 +0530

    cart count added, ajax added for add-to-cart

--- routes/index.js
+++ routes/index.js
@@ -1,71 +1,74 @@
-const { response } = require('express');
-var express = require('express');
+const { response } = require("express");
+var express = require("express");
 var router = express.Router();
-var productHelper=require('../helpers/productHelper')
-const userHelper=require('../helpers/userHelper')
+var productHelper = require("../helpers/productHelper");
+const userHelper = require("../helpers/userHelper");
 
-const verifyLogin=(req,res,next)=>{
-  if(req.session.loggedIn){
-    next()
-  }else{
-    res.redirect('/login')
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
   }
-}
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let user=req.session.user
+router.get("/", async function (req, res, next) {
+  let user = req.session.user;
   console.log(user);
-  productHelper.getAllProducts().then((products)=>{
-    res.render('user/viewProduct',{ products,user});
-  })
-
+  let cartCount = null;
+  if (req.session.user) {
+    cartCount = await userHelper.getCartCount(req.session.user._id);
+  }
+  productHelper.getAllProducts().then((products) => {
+    res.render("user/viewProduct", { products, user, cartCount });
+  });
 });
 
-router.get('/login',(req,res,next)=>{
-  if(req.session.loggedIn){
-    res.redirect('/')
-  }else{
-    res.render("user/login",{"loginError":req.session.logginError})
-    req.session.logginError="";
+router.get("/login", (req, res, next) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+  } else {
+    res.render("user/login", { loginError: req.session.logginError });
+    req.session.logginError = "";
   }
-})
-router.get('/signup',(req,res,next)=>{
-  res.render("user/signup")
-})
-router.post('/signup',(req,res,next)=>{
-  userHelper.doSignup(req.body).then((response)=>{
+});
+router.get("/signup", (req, res, next) => {
+  res.render("user/signup");
+});
+router.post("/signup", (req, res, next) => {
+  userHelper.doSignup(req.body).then((response) => {
     console.log(response);
-    req.session.loggedIn=true;
-    req.session.user=response
-    res.redirect('/')
-  })
-})
-router.post('/login',(req,res,next)=>{
-  userHelper.doLogin(req.body).then((response)=>{
-    if(response.status){
-      req.session.loggedIn=true;
-      req.session.user=response.user
-      res.redirect('/')
-    }else{
-      req.session.logginError="Invalid email or password"
-      res.redirect('/login')
+    req.session.loggedIn = true;
+    req.session.user = response;
+    res.redirect("/");
+  });
+});
+router.post("/login", (req, res, next) => {
+  userHelper.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true;
+      req.session.user = response.user;
+      res.redirect("/");
+    } else {
+      req.session.logginError = "Invalid email or password";
+      res.redirect("/login");
     }
-  })
-})
-router.get('/logout',(req,res,next)=>{
-  req.session.destroy()
-  res.redirect('/login')
-})
-router.get('/cart',verifyLogin,async (req,res,next)=>{
-  let products=await userHelper.getCartProducts(req.session.user._id)
+  });
+});
+router.get("/logout", (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/login");
+});
+router.get("/cart", verifyLogin, async (req, res, next) => {
+  let products = await userHelper.getCartProducts(req.session.user._id);
   console.log(products);
-  res.render("user/cart")
-})
-router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
-  userHelper.addtoCart(req.params.id,req.session.user._id).then(()=>{
-    res.redirect('/')
-  })
-})
-
+  res.render("user/cart", { products, user: req.session.user });
+});
+router.get("/add-to-cart/:id", (req, res, next) => {
+  console.log('api-call');
+  userHelper.addtoCart(req.params.id, req.session.user._id).then(() => {
+    res.json({status:true})
+  });
+});
 
 module.exports = router;

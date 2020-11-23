2573539c1430359e8122b61a916d0fd127da663b
Sarin M <Sarinmsari@gmail.com>
2020-11-23 22:34:49 +0530

    minor uneffective changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,13 @@
-addToCart=(proId)=>{
-    $.ajax({
-        url:'/add-to-cart/'+proId,
-        method:'get',
-        success:(response)=>{
-            if(response.status){
-                let count=$('#cart-count').html()
-                count=parseInt(count)+1
-                $('#cart-count').html(count)
-            }
-        }
-    })
+function addToCart(proId) {
+  $.ajax({
+    url: "/add-to-cart/" + proId,
+    method: "get",
+    success: (response) => {
+      if (response.status) {
+        let count = $("#cart-count").html();
+        count = parseInt(count) + 1;
+        $("#cart-count").html(count);
+      }
+    },
+  });
 }
